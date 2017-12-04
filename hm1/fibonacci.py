70a7f3694c76c206c43cf5e4dcee349fefb57272
polyanskyiow <polyanskyiow@gmail.com>
2017-12-04 18:54:00 +0200

    init commit

---
+++ hm1/fibonacci.py
@@ -0,0 +1,14 @@
+def fib(n):
+    a=0
+    b=1
+    for i in range(n):
+        if i==0:
+            print(a),
+        if i==1:
+            print(b),
+        if i>=2:
+            c=a+b
+            a=b
+            b=c
+            print(c),
+fib(11)
