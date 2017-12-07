9b06b5947a98d95fc3f40fb3149027a1457e0aee
polyanskyiow <polyanskyiow@gmail.com>
2017-12-07 15:07:36 +0200

    Add parametrized test

--- tests/test_numbers_pairs.py
+++ tests/test_numbers_pairs.py
@@ -1,13 +1,13 @@
+import pytest
 from my_app_tests.numbers_pairs import print_pairs
 
 def test_output_type():
-    assert type(print_pairs(10, 2,7,5,3,1,5,9,8,1)) is set
+    assert type(print_pairs(2,7,5,3,1,5,9,8,1)) is set
 
-def test_lengh_with_pairs():
-    assert len(print_pairs(10, 2,7,5,3,1,5,9,8,1)) == 4
-
-def test_lengh_with_no_pairs():
-    assert len(print_pairs(10, 1,2,3,4,5)) == 0
-
-def test_no_duplicates():
-    assert len(print_pairs(10, 5,5,5,5,5)) == 1
+@pytest.mark.parametrize("test_input,expected", [
+    ( (2,7,5,3,1,5,9,8,1) , 4),
+    ( (1,2,3,4,5) , 0),
+    ( (5,5,5,5,5) , 1)
+])
+def test_lengh_with_pairs(test_input, expected):
+    assert len(print_pairs(*test_input)) == expected
