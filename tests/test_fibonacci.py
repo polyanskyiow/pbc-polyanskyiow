dd05fe638cb916a3cfb46b09268009260abea5e4
Alexey Polianskii <polyanskyiow@gmail.com>
2017-12-07 03:40:38 +0200

    Add parametrized test

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,3 +1,4 @@
+import pytest
 from my_app_tests.fibonacci import fib
 
 def test_output_type():
@@ -6,11 +7,10 @@ def test_output_type():
 def test_lengh():
     assert len(fib(7)) == 7
 
-def test_float_as_argument():
-    assert len(fib(3.1)) == 0
-
-def test_sting_as_argument():
-    assert len(fib('string argument')) == 0
-
-def test_zero_as_argument():
-    assert len(fib(0)) == 0
+@pytest.mark.parametrize("test_input,expected", [
+    (3.1, 0),
+    ("string argument",0),
+    (0,0)
+])
+def test_arguments(test_input, expected):
+    assert len(fib(test_input)) == expected
