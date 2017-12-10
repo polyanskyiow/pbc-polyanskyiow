ba9ee006ff29dfe1f974292fa597a0f55d40b8f4
Alexey Polianskii <polyanskyiow@gmail.com>
2017-12-10 23:47:56 +0200

    Fixing #2 : adding markers for tests

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -1,16 +1,21 @@
 import pytest
 from my_app_tests.fibonacci import fib
 
+@pytest.mark.fibonacci
 def test_output_type():
     assert type(fib(7)) is list
 
+@pytest.mark.fibonacci
 def test_lengh():
     assert len(fib(7)) == 7
 
+@pytest.mark.fibonacci
 @pytest.mark.parametrize("test_input,expected", [
     (3.1, 0),
     ("string argument",0),
     (0,0)
 ])
+
+@pytest.mark.fibonacci
 def test_arguments(test_input, expected):
     assert len(fib(test_input)) == expected
